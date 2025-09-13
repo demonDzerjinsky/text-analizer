72ceb3634d56fc5132ec762bf829df9e8cd96f94
vtb4043344 <dcherkunov@vtb.ru>
2025-09-13 18:53:50 +0700

    change: call contract

--- src/main/java/ru/ssp/dto/ParamDto.java
+++ src/main/java/ru/ssp/dto/ParamDto.java
@@ -2,8 +2,10 @@ package ru.ssp.dto;
 
 /**
  * контракт вызова модуля поиска.
- * <p>
  *
+ * @param srcDir каталог содержащий текстовые файлы
+ * @param nWords количество слов в результирующей top
+ * @param nThreads максимальное количество потоков обработки
  */
-public record ParamDto() {
+public record ParamDto(String srcDir, int nWords, int nThreads) {
 }
