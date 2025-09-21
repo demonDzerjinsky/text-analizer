6d22e16ea600b3018d4dd4c3452ab3fa36528f23
dzerjinsky <dzerjinsky@mail.ru>
2025-09-21 11:21:34 +0700

    add: ThreadWordsAnalizerImpl design

---
+++ src/main/java/ru/ssp/core/impl/ThreadWordsAnalizerImpl.java
@@ -0,0 +1,24 @@
+package ru.ssp.core.impl;
+
+import java.util.List;
+
+import org.javatuples.Pair;
+
+import ru.ssp.core.ThreadWordsAnalizer;
+
+/**
+ * Реализует логику анализа текста на одном потоке.
+ */
+public final class ThreadWordsAnalizerImpl implements ThreadWordsAnalizer {
+
+    @Override
+    public List<Pair<String, Integer>> getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
+}
