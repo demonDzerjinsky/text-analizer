a901254ccc18601b66773c003f2a78c817ef4575
dzerjinsky <dzerjinsky@mail.ru>
2025-09-24 09:17:17 +0700

    add: getTop and transform
    completed merge and build result report block

--- src/main/java/ru/ssp/impl/TaskExecutorImpl.java
+++ src/main/java/ru/ssp/impl/TaskExecutorImpl.java
@@ -2,6 +2,9 @@ package ru.ssp.impl;
 
 import static java.util.Optional.of;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -50,7 +53,8 @@ class TaskExecutorImpl implements TaskExecutor {
             final List<String> fls, final int wds, final int ths) {
         return launch(fls, ths)
                 .flatMap(this::merge)
-                .flatMap(m -> this.getTop(m, wds));
+                .flatMap(m -> this.getTop(m, wds))
+                .flatMap(this::transform);
     }
 
     /**
@@ -98,16 +102,59 @@ class TaskExecutorImpl implements TaskExecutor {
     /**
      * определяет top-N слов.
      * кастомный метод, снижаем вычислительную сложность - уходим от
-     * полных сортировок и определяем элементы в один проход.
+     * полных сортировок больших коллекций и определяем только нужные элементы
+     * в один проход.
      *
      * @param mergedMap полная карта статистики всех слов
      * @param wds       количество TopN по скольки построить отчет
-     * @return отчет TopN
+     * @return отчет TopN в формате число-коллекция слов
      */
-    Optional<List<Pair<String, Integer>>> getTop(
-            final Map<String, Integer> mergedMap,
-            final int wds) {
-        return Optional.empty();
+    Optional<List<Pair<Integer, List<String>>>> getTop(
+            final Map<String, Integer> mergedMap, final int wds) {
+        final LinkedList<Pair<Integer, List<String>>> rpt = new LinkedList<>();
+        mergedMap.entrySet().forEach(it -> {
+            if (rpt.size() < wds || rpt.get(0).getValue0() <= it.getValue()) {
+                for (int i = 0; i < wds; i++) {
+                    if (i == rpt.size()) {
+                        rpt.add(new Pair<Integer, List<String>>(
+                                it.getValue(),
+                                Arrays.asList(it.getKey())));
+                        break;
+                    } else if (rpt.get(i).getValue0() == it.getValue()) {
+                        rpt.get(i).getValue1().add(it.getKey());
+                        break;
+                    } else if (rpt.get(i).getValue0() > it.getValue()) {
+                        rpt.add(i, new Pair<Integer, List<String>>(
+                                it.getValue(),
+                                Arrays.asList(it.getKey())));
+                        break;
+                    }
+                }
+                while (rpt.size() >= wds) {
+                    rpt.pop();
+                }
+            }
+        });
+        return of(rpt);
     }
 
+    /**
+     * преобразует отчет из внутреннего формата, полученного при выборке
+     * к формату контракта.
+     *
+     * @param rpt отчет во внутреннем формате
+     * @return отчет в формате контракта слово-частота
+     */
+    Optional<List<Pair<String, Integer>>> transform(
+            final List<Pair<Integer, List<String>>> rpt) {
+        final List<Pair<String, Integer>> result = new ArrayList<>();
+        for (var entry : rpt) {
+            for (int i = 0; i < entry.getValue1().size(); i++) {
+                result.add(
+                    new Pair<String, Integer>(
+                        entry.getValue1().get(i), entry.getValue0()));
+            }
+        }
+        return Optional.empty();
+    }
 }
